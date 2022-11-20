172cf424b55953ca49bd633cce1282f20350a2e6
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-11-20 21:31:14 +0500

    create the first test for flat json files

--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -1,13 +1,25 @@
 <?php
-
 namespace Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use function Differ\Differ\gendiff;
+use function Differ\Differ\genDiff;
 
 class gendiffTest extends TestCase
 {
-    
-}
+    public function getFixtureFullPath($fixtureName)
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
+    }
 
+    public function test()
+    {
+        $file1 = $this->getFixtureFullPath('file1.json');
+        $file2 = $this->getFixtureFullPath('file2.json');
+        $result = file_get_contents($this->getFixtureFullPath('resultJsonFiles.json'));
+
+        $diff = genDiff($file1, $file2);
+        return $this->assertTrue(genDiff($file1, $file2) == $result);
+    }
+}
