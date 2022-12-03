ae06f7795179928ea3a07898a98b8c9f84c08fe7
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-12-04 01:02:57 +0500

    update Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -10,49 +10,9 @@ function genDiff($pathToFile1, $pathToFile2)
     $file1Content = parseFile(realpath($pathToFile1));
     $file2Content = parseFile(realpath($pathToFile2));
 
-    $result = getLines($file1Content, $file2Content);
+    $result = getUniqueKeysOfFiles($file1Content, $file2Content);
 
-    return "{\n$result\n}\n";
-}
-
-function getLines($file1Content, $file2Content)
-{
-    $iter = function ($file1Content, $file2Content) {
-        $filesKeys = getUniqueKeysOfFiles($file1Content, $file2Content);
-        $result = array_map(
-            function ($key) use ($file1Content, $file2Content) {
-                return makeDifferenceCheck($file1Content, $file2Content, $key);
-            },
-            $filesKeys
-        );
-        return implode("\n", $result);
-    };
-    $result = $iter($file1Content, $file2Content);
-    return $result;
-}
-
-function makeDifferenceCheck($file1Content, $file2Content, $key)
-{
-    if (!array_key_exists($key, $file2Content)) {
-        $file1Value = toString($file1Content[$key]);
-        var_dump($file1Value);
-        return "- {$key}: {$file1Value}";
-    }
-    if (!array_key_exists($key, $file1Content)) {
-        $file2Value = toString($file2Content[$key]);
-        return "+ {$key}: {$file2Value}";
-    }
-    $file1Value = $file1Content[$key];
-    $file2Value = $file2Content[$key];
-    if (!is_array($file1Value) || !is_array($file2Value)) {
-        $file1Value = toString($file1Content[$key]);
-        $file2Value = toString($file2Content[$key]);
-        if ($file1Value !== $file2Value) {
-            return "- {$key}: {$file1Value}" . "\n" . "+ {$key}: {$file2Value}";
-        }
-            return "  {$key}: {$file1Value}";
-    }
-    return "  {$key}:\n" . getLines($file1Value, $file2Value);
+    return json_encode($result);
 }
 
 function getUniqueKeysOfFiles($file1Content, $file2Content)
@@ -61,7 +21,51 @@ function getUniqueKeysOfFiles($file1Content, $file2Content)
     $file2Keys = array_keys($file2Content);
     $filesKeys = array_unique(array_merge($file1Keys, $file2Keys));
     sort($filesKeys, SORT_STRING);
-    return $filesKeys;
+    return array_map(fn($key) => makeDifferenceCheck($file1Content, $file2Content, $key), $filesKeys);;
+}
+
+function makeDifferenceCheck($file1Content, $file2Content, $key)
+{
+    $file1Value = $file1Content[$key] ?? null;
+    $file2Value = $file2Content[$key] ?? null;
+    if (!array_key_exists($key, $file2Content)) {
+        $value = getLines($file1Value);
+        return ["-",  $key, $value];
+    }
+    if (!array_key_exists($key, $file1Content)) {
+        $value = getLines($file2Value);
+        return ["+",  $key, $value];
+    }
+    if (is_array($file1Value) && is_array($file2Value)) {
+        $value = getLines($file1Content);
+        return [" ",  $key, $value];
+    }
+    if ($file1Value !== $file2Value) {
+        return ["-",  $key, $file1Value, "+",  $key, $file2Value,];
+    }
+    return [" ",  $key, $file1Value];
+}
+
+function getLines($fileContent)
+{
+    $iter = function ($fileContent) use (&$iter){
+        if (!is_array($fileContent)) {
+            return toString($fileContent);
+        }
+
+        $filesKeys = array_keys($fileContent);
+        return array_map(
+            function ($key) use ($fileContent, $iter) {
+                $value = $fileContent[$key];
+                if (is_array($value)) {
+                    $value = $iter($value);
+                }
+                return $value;
+            },
+            $filesKeys
+        );
+    };
+    return $iter($fileContent);
 }
 
 function toString($value)
