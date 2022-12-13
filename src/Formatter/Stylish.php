83198ab27f98116113c7675f422f054079133c38
Гульшат Нигматуллина <nigma1918@mail.ru>
2022-12-13 17:33:46 +0500

    update Stylish.php

--- src/Formatter/Stylish.php
+++ src/Formatter/Stylish.php
@@ -14,37 +14,40 @@ function getFormat($tree, $depth = 1)
             if (is_array($value)) {
                 $value = $iter($value, $depth + 2);
             }
-
-            if ($type === "changed") {
-                $value2 = getValue2($node);
-                if ($value === "") {
-                    return "$indentStart- $key:" . "\n" . "$indentStart+ $key: $value2";
-                }
-                return "$indentStart- $key: $value" . "\n" . "$indentStart+ $key: $value2";
-            }
-
-            if ($type  === "parent node") {
-                return "$indentStart  $key: $value";
-            }
-
-            if ($type === "deleted") {
-                return "$indentStart- $key: $value";
-            }
-
-            if ($type === "added") {
-                return "$indentStart+ $key: $value";
-            }
-
-            if ($type === "unchanged") {
-                return "$indentStart  $key: $value";
-            }
+            return getResultByType($type, $indentStart, $key, $value, $node);
         }, $tree);
         $result = ["{", ...$lines, "{$indentEnd}}"];
         return implode("\n", $result);
     };
     $line = $iter($tree, 1);
-    var_dump($line);
     return $line;
+}
+
+function getResultByType($type, $indentStart, $key, $value, $node)
+{
+    switch ($type) {
+        case "parent node":
+            $result =  "$indentStart  $key: $value";
+            break;
+        case "changed":
+            $value2 = getValue2($node);
+            $line1 = "$indentStart- $key:" . "\n" . "$indentStart+ $key: $value2";
+            $line2 = "$indentStart- $key: $value" . "\n" . "$indentStart+ $key: $value2";
+            $result = $value === "" ? $line1 : $line2;
+            break;
+        case "deleted":
+            $result = "$indentStart- $key: $value";
+            break;
+        case "added":
+            $result = "$indentStart+ $key: $value";
+            break;
+        case "unchanged":
+            $result = "$indentStart  $key: $value";
+            break;
+        default:
+            break;
+    }
+        return $result;
 }
 
 function getCategory($node)
